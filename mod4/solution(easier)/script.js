7cb403e3a504970ea07779d99f5681b007797bbb
atharvb13 <85288007+atharvb13@users.noreply.github.com>
2021-08-30 19:43:04 +0530

    Add files via upload

---
+++ mod4/solution(easier)/script.js
@@ -0,0 +1,17 @@
+//look at script.js on github for all instructions
+(function () {
+
+   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+   for (var i = 0; i < names.length; i++) {
+
+    
+    var firstLetter = names[i].charAt(0).toLowerCase();
+  
+    if (firstLetter === 'j') {
+      byeSpeaker(names[i]);
+    } else {
+      helloSpeaker(names[i]);
+    }
+  }
+  
+  })();
